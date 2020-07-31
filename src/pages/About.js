9275574bddf9277adba79df92ebe1fc889350ca7
mvictorl <mvictorl@gmail.com>
2020-07-31 10:53:02 +0300

    Fix Alert for switch pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,18 @@
 import React from "react"
 
-export const About = () => (
-  <div className='jumbotron'>
-    <div className='container'>
-      <h1 className='display-4'>About Note App</h1>
-      <p className='lead'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, animi temporibus reprehenderit aliquam numquam blanditiis id ullam rem facere, voluptates labore velit earum! Corrupti eos laboriosam quis doloremque facere debitis unde necessitatibus aliquid fugit sint!
-      </p>
+export const About = () => {
+
+  return (
+    <div className='jumbotron'>
+      <div className='container'>
+        <h1 className='display-4'>About Note App</h1>
+        <p className='lead'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          animi temporibus reprehenderit aliquam numquam blanditiis id ullam rem
+          facere, voluptates labore velit earum! Corrupti eos laboriosam quis
+          doloremque facere debitis unde necessitatibus aliquid fugit sint!
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,32 @@ import React, {useContext, useEffect} from "react"
 import { Form } from "../components/Form"
 import { Notes } from "../components/Notes"
 import { FirebaseContext } from "../context/firebase/firebaseContext"
+import { AlertContext } from "../context/alert/alertContext"
 import { Loader } from "../components/Loader"
+import { Alert } from "../components/Alert"
 
 export const Home = () => {
   // const notes = new Array(3).fill("").map((_, i) => ({id: i, title: `Note ${i+1}`}))
   const {notes, loading, fetchNotes, removeNote} = useContext(FirebaseContext)
+  const {hide} = useContext(AlertContext)
 
+  // Hook = ComponentDidMount (once)
   useEffect(() => {
     fetchNotes()
     // eslint-disable-next-line
   }, [])
 
+  // Hook = ComponentWillUnmount
+  useEffect(() => {
+    return () => {
+      hide()
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
+      <Alert />
       <Form />
       <hr />
       {loading
